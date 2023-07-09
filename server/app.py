667328cf161852342265733b3ccf479dfa958bd8
Macolister <bispo.mac@gmail.com>
2023-07-09 16:07:46 -0400

    finish tests

--- server/app.py
+++ server/app.py
@@ -17,11 +17,60 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+
+            response_dict_list = [plant.to_dict() for plant in Plant.query.all()]
+
+            response = make_response(
+                response_dict_list,
+                200,
+            )
+
+            return response
+
+    def post(self):
+        request_json = request.get_json()
+        new_plant = Plant(
+            name=request_json['name'],
+            image=request_json['image'],
+            price=request_json['price']
+        )
+        # name = db.Column(db.String)
+        # image = db.Column(db.String)
+        # price = db.Column(db.Float)
+
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response_dict = new_plant.to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            201,
+        )
+
+        return response
+
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+
+        response_dict = Plant.query.filter(Plant.id==id).first().to_dict()
+
+        response = make_response(
+            response_dict,
+            200,
+        )
+
+        return response
+    
+
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
